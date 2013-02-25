273ab237d3c758de3be55a4b320af32b4b2289bc
XU Kai <xukai.ken@gmail.com>
2013-02-26 00:07:13 +0800

    dev

--- src/modular.js
+++ src/modular.js
@@ -2,17 +2,20 @@
  * A basic AMD module management system for the web
  * @author XU Kai(xukai.ken@gmail.com)
  * @version 0.1 initialise the base framework
- * 
+ *
  */
 
 :(function(global) {
-	
-	global.define = function() {
-		
+	Modular = {};
+
+	var modules = {};
+
+	define = function() {
+
 	};
 
-	global.require = function() {
-		
+	require = function() {
+
 	};
 
-})(this)
+})(this);
---
+++ src/utils.js
@@ -0,0 +1,14 @@
+/**
+ * A basic AMD module management system for the web: utils types.
+ * @author XU Kai(xukai.ken@gmail.com)
+ * @version 0.1 initialise the base framework
+ *
+ */
+
+;(function(global) {
+
+	global.formatArray = function() {
+		
+	};
+
+})(Modular);
