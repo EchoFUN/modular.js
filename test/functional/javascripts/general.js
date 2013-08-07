a5de7c0263772c87ec9be61842e68bfa686e33de
XU Kai <xukai.ken@gmail.com>
2013-08-07 15:25:34 +0800

    Finish the test cases first.

---
+++ test/functional/javascripts/general.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 
+ * 
+ */
+
+define(['base', 'base2'], function(require) {
+   
+   var base = require('base');
+   var base2 = require('base2');
+   
+   console.log(base.toString() + base1.toString());
+   
+});
---
+++ test/functional/javascripts/modules/base.js
@@ -0,0 +1,7 @@
+define(function(require, exports) {
+   
+   return {
+      'a': 'b' 
+   };
+   
+});
